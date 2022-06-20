06ee45b3be6b84cb4cd7dba93ee378595e08b58d
suho2001228 <103232995+suho2001228@users.noreply.github.com>
2022-06-20 05:53:52 -0900

    Add files via upload

---
+++ subscriber.py
@@ -0,0 +1,52 @@
+import random
+import time
+import paho.mqtt.client as mqtt_client
+
+broker_address = "localhost"
+broker_port = 1883
+
+topic = "ultra"
+
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker")
+        else:
+            print(f"Failed to connect, Returned code: {rc}")
+
+    def on_disconnect(client, userdata, flags, rc=0):
+        print(f"disconnected result code {str(rc)}")
+
+    def on_log(client, userdata, level, buf):
+        print(f"log: {buf}")
+
+    # client 생성
+    client_id = f"mqtt_client_{random.randint(0, 1000)}"
+    client = mqtt_client.Client(client_id)
+
+    # 콜백 함수 설정
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_log = on_log
+
+    # broker 연결
+    client.connect(host=broker_address, port=broker_port)
+    return client
+
+
+def subscribe(client: mqtt_client): # xxx
+    def on_message(client, userdata, msg):
+        print(f"displacement = `{msg.payload.decode()}`")
+    client.subscribe(topic) #1
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
